3297ca3fc19f1295b524344c50519bac22e12813
Austin Gill <Notgnoshi@gmail.com>
2025-07-19 11:38:31 -0500

    Add libemu.la library and asm binary

--- asm.c
+++ asm.c
@@ -1,9 +1,12 @@
 #include "config.h"
+#include "emu/add.h"
 
 #include <stdio.h>
 
 int main(void)
 {
     printf(PACKAGE_STRING "\n");
+    int result = emu_add(2, 2);
+    printf("result: %d\n", result);
     return 0;
 }
---
+++ src/add.c
@@ -0,0 +1,6 @@
+#include "emu/add.h"
+
+int emu_add(int a, int b)
+{
+    return a + b;
+}
